793526f6bc4f9035f5af2bddca6fd6c8060bb7d2
Raúl Negro Carpintero <rnc0011@alu.ubu.es>
2017-03-07 10:50:12 +0100

    Add files via upload
    Se han añadido los archivos dentro de la carpeta src.

---
+++ src/Billiards.java
@@ -0,0 +1,125 @@
+package p012;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+@SuppressWarnings("serial")
+public class Billiards extends JFrame {
+
+	public static int Width = 800;
+	public static int Height = 600;
+
+	private JButton b_start, b_stop;
+	
+	private Board board;
+
+	// TODO update with number of group label. See practice statement.
+	private final int N_BALL = 3 + 3;
+	
+	private Ball[N_BALL] balls = new Ball[this.N_BALL];
+	protected Thread[N_BALL] threads = new Thread[this.N_BALL];
+
+	public Billiards() {
+
+		board = new Board();
+		board.setForeground(new Color(0, 128, 0));
+		board.setBackground(new Color(0, 128, 0));
+
+		initBalls();
+
+		b_start = new JButton("Empezar");
+		b_start.addActionListener(new StartListener());
+		b_stop = new JButton("Parar");
+		b_stop.addActionListener(new StopListener());
+
+		JPanel p_Botton = new JPanel();
+		p_Botton.setLayout(new FlowLayout());
+		p_Botton.add(b_start);
+		p_Botton.add(b_stop);
+
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(board, BorderLayout.CENTER);
+		getContentPane().add(p_Botton, BorderLayout.PAGE_END);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(Width, Height);
+		setLocationRelativeTo(null);
+		setTitle("Práctica programación concurrente objetos móviles independientes");
+		setResizable(false);
+		setVisible(true);
+	}
+
+	private void initBalls() {
+		for (i = 0; i < this.N_BALL; i++){
+			balls[i] = new Ball();
+		}
+	}
+	
+	protected Thread makeThread (final Ball bola){
+		Runnable bucle = new Runnable() {
+			public void run(){
+				try {
+					for(;;) {
+						bola.move();
+						bola.reflect();
+						board.paint(getGraphics());
+					}
+				} catch (InterruptedException e){
+					return;
+				}
+			}
+		};
+		return new Thread (bucle);
+	}
+	
+	public synchronized void start () {
+		if (threads == null) {
+			this.initBalls();
+			this.board.setBalls(balls);
+			for (int i = 0; i < this.N_BALL;i++){
+				this.threads[i] = makeThread(balls[i])
+				this.threads[i].start(); 
+			}
+		} 
+	}
+	
+	public synchronized void stop() {
+		if (threads != null) {
+			for (int i; i < threads.lenght; i++){
+				threads[1].interrupt();
+				threads = null;
+			}
+		}
+	}
+	
+	public void finalize(){
+		this.stop();
+	}
+	
+	private class StartListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Code is executed when start button is pushed
+			this.start();
+		}
+	}
+
+	private class StopListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Code is executed when stop button is pushed
+			this.finalize();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Billiards();
+	}
+}
